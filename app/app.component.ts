7a55ff9e07c5646f86b60a3434ec51498b1bd976
Andrew Niekamp and Russ Davies <andrew+russ@>
2016-08-23 15:39:00 -0700

    add initial file structure and initial components

---
+++ app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'keg-display',
+  inputs: ['keg'],
+  template: `
+    <h3>
+      {{ keg.name }} -
+      {{ keg.brand }} -
+      {{ keg.price }} -
+      {{ keg.alcoholContent }}
+    </h3>
+  `
+})
+export class KegComponent {
+  public keg: Keg;
+}
+
+@Component({
+  selector: 'keg-list',
+  inputs: ['kegList'],
+  outputs: ['onKegSelect'],
+  directives: [KegComponent],
+  template: `
+  <keg-display *ngFor="#currentKeg of kegList"
+    (click)="kegClicked(currentKeg)"
+    [class.selected]="currentKeg === selectedKeg"
+    [keg]="currentKeg">
+  </keg-display>
+  `
+})
+export class KegListComponent {
+  public kegList: Keg[];
+  public onKegSelect: EventEmitter<Keg>;
+  public selectedKeg: Keg;
+  constructor() {
+    this.onKegSelect = new EventEmitter();
+  }
+  kegClicked(clickedKeg: Keg): void {
+    console.log("child", clickedKeg);
+    this.selectedKeg = clickedKeg;
+    this.onKegSelect.emit(clickedKeg);
+  }
+}
+
+@Component({
+  selector: 'my-app',
+  directives: [KegListComponent],
+  template: `
+    <div class="container">
+      <h1>Brews</h1>
+      <keg-list
+        [kegList]="kegs"
+        (onKegSelect)="kegWasSelected($event)">
+      </keg-list>
+    </div>
+  `
+})
+export class AppComponent {
+  public kegs: Keg[];
+  constructor(){
+    this.kegs = [
+      new Keg("Dragon's Blood Vermillion Ale", "Highpass Brewing Co.", 8, "6.5%"),
+      new Keg("Excalibur", "Humble Monk Distillery", 9, "7%"),
+      new Keg("Eye of Newt Brew", "Witch's Tooth", 5, "12.5%")
+    ];
+  }
+  kegWasSelected(clickedKeg: Keg): void {
+    console.log('parent', clickedKeg);
+  }
+}
